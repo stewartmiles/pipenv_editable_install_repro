86ed880900ee95eb1419bb3aab928a94ca00759c
Stewart Miles <stewart@agentic.ai>
2023-08-29 11:02:22 -0700

    Ensure relative paths are stored in Pipfile / Pipfile.lock

--- try_pipenv_versions.sh
+++ try_pipenv_versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xeu
 
 readonly PIPENV_VERSIONS=(
-  2023.7.23
+  2023.7.23 # Works
   # Ignore known broken versions.
   # 2023.8.19
   # 2023.8.20
@@ -11,6 +11,30 @@ readonly PIPENV_VERSIONS=(
   2023.8.28
 )
 
+check_pipfile() {
+  local relative_package_path="${1}"
+  local alternative_package_path=$(echo "${relative_package_path}" | \
+                                     sed -r 's@^\./@@')
+  local package_name=$(echo "${relative_package_path}" | \
+                         sed -r 's@.*/([^/]+)@\1@')
+  local error=0
+  for filename in Pipfile Pipfile.lock; do
+    local found_relative_package_path="$(\
+      grep -F "\"${relative_package_path}" "${filename}")"
+    local found_alternative_package_path="$(\
+      grep -F "\"${alternative_package_path}" "${filename}")"
+    if [[ -z "${found_relative_package_path}" &&
+          -z "${found_alternative_package_path}" ]]; then
+      echo "${relative_package_path} and ${alternative_package_path} " >&2
+      echo "not found in {filename}" >&2
+      echo "--- ${filename} ---" >&2
+      cat "${filename}" >&2
+      error=1
+    fi
+  done
+  return $((error))
+}
+
 install_test_and_uninstall_package() {
   local venv_path="${1}"
   local package_path="${2}"
@@ -19,11 +43,13 @@ install_test_and_uninstall_package() {
   if ! pipenv run pipenv install -e "${package_path}"; then
     echo "Installation of ${path} failed" >&2
     error=1
+  elif ! check_pipfile "${package_path}"; then
+    error=1
   elif ! pipenv run test-hello; then
     echo 'Execution of test-hello failed.' >&2
     error=1
   elif ! pipenv uninstall test-hello; then
-    echo 'Uninstallation of test-hello failed.'
+    echo 'Uninstallation of test-hello failed.' >&2
     error=1
   fi
   popd
@@ -36,6 +62,7 @@ main() {
     pipx uninstall pipenv || true
     pipx install "pipenv==${version}"
     git clean -dfx
+    git checkout Pipfile{,.lock} another_venv/Pipfile{,.lock}
     echo "=== Using pipenv version ${version} ==="
     pipenv sync
     if ! ( install_test_and_uninstall_package \
@@ -43,9 +70,9 @@ main() {
            install_test_and_uninstall_package \
              . ./applications/test-hello &&
            install_test_and_uninstall_package \
-             another_venv ../test-hello &&
+             another_venv ./../test-hello &&
            install_test_and_uninstall_package \
-             another_venv ../applications/test-hello ); then
+             another_venv ./../applications/test-hello ); then
       echo "--- pipenv version ${version} is broken ---"
     else
       echo "--- pipenv version ${version} is working ---"
