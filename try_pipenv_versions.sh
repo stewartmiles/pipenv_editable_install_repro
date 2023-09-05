2ebd47a83ca218bdc3fc1536682c0733f44f5a08
Stewart Miles <stewart@agentic.ai>
2023-09-05 11:48:07 -0700

    Test pipenv with different configuration via a `.env` file.

--- try_pipenv_versions.sh
+++ try_pipenv_versions.sh
@@ -12,17 +12,40 @@ readonly PIPENV_VERSIONS=(
   2023.9.1
 )
 
+# Written into .env before each set of executed tests.
+readonly DOTENV=(
+  ""
+
+  # Add a local directory for wheel search.
+  "PIP_FIND_LINKS=${PWD}/wheels
+"
+
+  # Also, disable pip build isolation to allow editable packages to depend upon
+  # each other at build time.
+  "PIP_FIND_LINKS=${PWD}/wheels
+PIP_NO_BUILD_ISOLATION=0
+"
+  
+  # Also, disable locking so that multiple packages can be installed using
+  # pipenv install time and the all locked at once.
+  "PIP_FIND_LINKS=${PWD}/wheels
+PIP_NO_BUILD_ISOLATION=0
+PIPENV_SKIP_LOCK=true
+"
+)
+
 check_pipfile() {
   local relative_package_path="${1}"
-  local alternative_package_path=$(echo "${relative_package_path}" | \
-                                     sed -r 's@^\./@@')
-  local package_name=$(echo "${relative_package_path}" | \
-                         sed -r 's@.*/([^/]+)@\1@')
+  local alternative_package_path
+  alternative_package_path=$(echo "${relative_package_path}" | \
+                               sed -r 's@^\./@@')
   local error=0
   for filename in Pipfile Pipfile.lock; do
-    local found_relative_package_path="$(\
+    local found_relative_package_path
+    found_relative_package_path="$(\
       grep -F "\"${relative_package_path}" "${filename}")"
-    local found_alternative_package_path="$(\
+    local found_alternative_package_path
+    found_alternative_package_path="$(\
       grep -F "\"${alternative_package_path}" "${filename}")"
     if [[ -z "${found_relative_package_path}" &&
           -z "${found_alternative_package_path}" ]]; then
@@ -42,7 +65,7 @@ install_test_and_uninstall_package() {
   local error=0
   pushd "${venv_path}"
   if ! pipenv run pipenv install -e "${package_path}"; then
-    echo "Installation of ${path} failed" >&2
+    echo "Installation of ${package_path} failed" >&2
     error=1
   elif ! check_pipfile "${package_path}"; then
     error=1
@@ -57,31 +80,53 @@ install_test_and_uninstall_package() {
   return $((error))
 }
 
+make_venv() {
+  local venv_directory="${1}"
+  local dotenv_contents="${2}"
+  mkdir -p "${venv_directory}"
+  (
+    cd "${venv_directory}"
+    pwd
+    echo -e "${dotenv_contents}" > .env
+    pipenv --python "$(command -v python)"
+  )
+}
+
 main() {
   cd "$(dirname "$0")"
   for version in "${PIPENV_VERSIONS[@]}"; do
     pipx uninstall pipenv || true
     pipx install "pipenv==${version}"
-    git clean -dfx
     echo "=== Using pipenv version ${version} ==="
-    pipenv --python $(which python)
-    (
-      mkdir -p another_venv
-      cd another_venv
-      pipenv --python $(which python)
-    )
-    if ! ( install_test_and_uninstall_package \
-             . ./test-hello &&
-           install_test_and_uninstall_package \
-             . ./applications/test-hello &&
-           install_test_and_uninstall_package \
-             another_venv ./../test-hello &&
-           install_test_and_uninstall_package \
-             another_venv ./../applications/test-hello ); then
-      echo "--- pipenv version ${version} is broken ---"
-    else
-      echo "--- pipenv version ${version} is working ---"
-    fi
+
+    for dotenv_contents in "${DOTENV[@]}"; do
+      git clean -dfx
+      echo -e "--- Using .env---\n${dotenv_contents}"
+
+      local venv_directory
+      local relative_root_directory
+      for vars in 'venv_directory=.; relative_root_directory=.' \
+                  'venv_directory=another_venv; relative_root_directory=..'; do
+        eval "${vars}"
+        echo "--- Installing packages in ${venv_directory} ---"
+        # NOTE: To nest these, venvs in subdirectories need to be created first
+        # otherwise pipenv walks the parent tree and finds the wrong venv.
+        make_venv "${venv_directory}" "${dotenv_contents}"
+
+        # Test package installation.
+        if ! ( install_test_and_uninstall_package \
+                 "${venv_directory}" \
+                 "${relative_root_directory}/test-hello" && \
+               install_test_and_uninstall_package \
+                 "${venv_directory}" \
+                 "${relative_root_directory}/applications/test-hello" ); then
+          echo "--- pipenv version ${version} is broken ---"
+          break
+        else
+          echo "--- pipenv version ${version} is working ---"
+        fi
+      done
+    done
   done
 }
 
