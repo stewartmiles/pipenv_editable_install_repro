d4c7374b801694eec7b351723732ce996a5d4112
Stewart Miles <stewart@agentic.ai>
2023-09-08 10:23:20 -0700

    Clean up requires-setuptools/install.sh and add install load test

--- requires-setuptools/install_multiple.sh
+++ requires-setuptools/install_multiple.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -eu
-# Flip PIP_NO_BUILD_ISOLATION to 1 fix the installation with pipenv 2023.9.7
-export PIP_NO_BUILD_ISOLATION=0;
+: "${INSTALL_PACKAGES:=50}"
+
 export PIPENV_SKIP_LOCK=true;
 export PIPENV_VENV_IN_PROJECT=true;
 cd "$(cd "$(dirname "${0}")"; pwd)/.."
 git clean -dfx
 pipenv --python "$(command -v python)"
 declare -a pipenv_install_args=()
-for i in $(seq 1 50); do
+for i in $(seq 1 $((INSTALL_PACKAGES))); do
   package_name=requires-setuptools$((i))
   cp -r ./requires-setuptools "./${package_name}"
   sed -i "s/requires-setuptools/${package_name}/g" \
